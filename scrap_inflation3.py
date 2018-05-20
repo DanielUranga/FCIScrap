c0fa1a0b5cd69fb25cdd4e59adcbfe7f861da81f
fersbery <fersbery@localhost.localdomain>
2018-05-20 00:36:21 -0300

    Scrap data from  www.bcra.gov.ar

---
+++ scrap_inflation3.py
@@ -0,0 +1,42 @@
+#!/bin/env python
+
+import subprocess
+from bs4 import BeautifulSoup
+import csv
+import datetime
+import time
+from datetime import timedelta
+
+command = [
+    'curl',
+    'http://www.bcra.gov.ar/PublicacionesEstadisticas/Principales_variables_datos.asp',
+    '--data',
+    'desde=31%2F03%2F2016&hasta=31%2F04%2F2016&fecha=Fecha_Cvs&descri=22&campo=Cvs&primeravez=1&alerta=5'
+]
+
+html = subprocess.check_output(command)
+soup = BeautifulSoup(html, 'html.parser')
+
+table = soup.select('#tabla')[0]
+rows = table.find_all('tr')
+data = []
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.insert(0, [ele for ele in cols if ele])
+
+with open('data/inflation_data.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=['date', 'value'])
+    writer.writeheader()
+    consecutive_date = None
+    for row in data:
+        if not row:
+            continue
+        st_time = time.strptime(row[0], '%d/%m/%Y')
+        date = datetime.date(st_time.tm_year, st_time.tm_mon, st_time.tm_mday)
+        if consecutive_date == None:
+            consecutive_date = date
+        while consecutive_date < date:
+            consecutive_date = consecutive_date + timedelta(days=1)
+            float_val = float(row[1].replace(',', '.'))
+            writer.writerow({'date': consecutive_date.strftime('%d/%m/%Y'), 'value': float_val})
